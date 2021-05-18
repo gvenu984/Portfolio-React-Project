000391cd82e20ee2579488b3d70708ddc7fd87f7
gvenu984 <gvenu984@gmail.com>
2021-05-18 07:07:01 +0530

    adding HOC Patterns

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -18,8 +18,9 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         if (!userAuth) return;
 
         const userRef = firestore.doc(`users/${userAuth.uid}`);
-
+        
         const snapShot = await userRef.get();
+        
 
         if(!snapShot.exists) {
            const { displayName, email } = userAuth;
@@ -40,8 +41,37 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         return userRef;
 };
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+        const collectionRef = firestore.collection(collectionKey);
 
+        const batch = firestore.batch();
+        objectsToAdd.forEach(obj => {
+                const newDocRef = collectionRef.doc();
+                batch.set(newDocRef, obj);
+        });
+
+       return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap=(collections)=> {
+        const transformedCollection = collections.docs.map(doc => {
+                const { title, items } = doc.data();
+
+                return {
+                        routeName: encodeURI(title.toLowerCase()),
+                        id: doc.id,
+                        title,
+                        items
+                }
+        });
+        
+        return transformedCollection.reduce((accumulator, collection) => {
+                accumulator[collection.title.toLowerCase()] = collection;
+                return accumulator;
+        } , {});
+}
+
+        firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
